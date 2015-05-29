0089d2ef2dbff0e9ec21bbfb3f795afd696a2c48
Andres Lozada Mosto <alfathenus@gmail.com>
2015-05-29 04:35:48 -0300

    asd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('slackin')({
-  token: 'xoxp-5108888872-5108888878-5130468579-c30317', // required
+  token: 'SLACK_KEY', // required
   interval: 1000,
   org: 'meetupphpbuenosaires', // required
   //channels: 'channel,channel,...' // for single channel mode
