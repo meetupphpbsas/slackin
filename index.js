ca4a63e519756c812275575fe3708f995f602873
Andres Lozada Mosto <alfathenus@gmail.com>
2015-05-29 03:50:21 -0300

    asd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('slackin')({
   token: 'xoxp-2978261920-2980861454-5108392512-7d0bb1', // required
   interval: 1000,
-  org: 'meetPHPar', // required
+  org: 'meetphpar', // required
   //channels: 'channel,channel,...' // for single channel mode
   silent: false // suppresses warnings
 }).listen(process.env.PORT);
